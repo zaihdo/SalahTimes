c03cd29caf6f85c79438a30e44372f1df33b9f0b
Zaidh Imran <zaidhimran2000@gmail.com>
2024-07-25 10:24:49 -0230

    minor changes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -52,7 +52,7 @@ export default function TabLayout() {
                   <FontAwesome
                     name="info-circle"
                     size={25}
-                    color={Colors[colorScheme ?? 'light'].background}
+                    color={Colors[colorScheme ?? 'light'].tint}
                     style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
                   />
                 )}
