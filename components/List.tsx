5ec1b44883137e3933f2786b38b5e092d7492fbe
Zaidh Imran <zaidhimran2000@gmail.com>
2024-08-05 08:59:37 -0230

    rendered sql data on screen with List

--- components/List.tsx
+++ components/List.tsx
@@ -2,20 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { FlatList } from 'react-native';
 import ListItem from './ListItem';
 import { StyleSheet } from 'react-native';
-import { format, parse } from 'date-fns';
 import { IqamahTime } from '@/types/dbTypes';
 import { useSQLiteContext } from 'expo-sqlite/next';
-
-const prayerTimesData = {
-    "Fajr": "04:30 AM",
-    "Sunrise": "06:00 AM",
-    "Dhuhr": "01:00 PM",
-    "Asr Shafiee": "03:30 PM",
-    "Asr Hanafee": "05:00 PM",
-    "Maghrib": "08:30 PM",
-    "Esha": "10:00 PM",
-    "Sunset": "08:30 PM"
-  };
 
 export default function List() {
   const [iqamahs, setIqamahs] = useState<IqamahTime[]>([]);
@@ -24,17 +12,11 @@ export default function List() {
 
   useEffect(() => {
     db.withTransactionAsync(async () => {
-      // getData();
       iqamahQuery();
       
     });
   }, [db])
   
-  async function getData() {
-    const result = await db.getAllAsync(`SELECT * FROM Iqamahs WHERE `);
-    console.log(result, "huh");
-  }
-
   function formatDateQuery() {
     const today = new Date();
     const day = today.getDate();
@@ -45,26 +27,25 @@ export default function List() {
   async function iqamahQuery() {
     const date = formatDateQuery();
     const masjid = "FRANCISTOWN MASJID";
-    const results: IqamahTime[] = db.getAllSync(`SELECT Fajr, Dhuhr, DhuhrSunday, Asr, Maghrib, Isha FROM Iqamahs WHERE Date = ? AND Masjid = ?`, [date, masjid]);
+    const results = db.getAllSync<IqamahTime>(`SELECT Fajr, Dhuhr, DhuhrSunday, Asr, Maghrib, Isha FROM Iqamahs WHERE Date = ? AND Masjid = ?`, [date, masjid]);
     setIqamahs(results);
-    results.forEach(row => {
-      console.log(`Fajr: ${row.Fajr}`);
-      console.log(`Dhuhr: ${row.Dhuhr}`);
-    });
   }
-  const currentDate = new Date();
-  const data = Object.entries(prayerTimesData).map(([prayer, time]) => {
-    const dateTime = parse(time, 'hh:mm a', currentDate);
-    return { prayer, time, dateTime };
-  });
+
+  const formatColumnName = (name: string) => {
+    return name.replace(/([a-z])([A-Z])/g, '$1-$2');
+  };
+
+  const data = iqamahs.flatMap(iqamah => Object.entries(iqamah));
+
+  const renderItem = ({ item }: { item: [string, string] }) => (
+    <ListItem prayer={formatColumnName(item[0])} time={item[1]}></ListItem>
+  );
 
   return (
     <FlatList
       data={data}
-      keyExtractor={item => item.prayer}
-      renderItem={({ item }) => (
-        <ListItem prayer={item.prayer} time={format(item.dateTime, 'hh:mm a')} />
-      )}
+      keyExtractor={(item) => item[0]}
+      renderItem={renderItem}
       contentContainerStyle={styles.listContainer}
       scrollEnabled={false}
     />
