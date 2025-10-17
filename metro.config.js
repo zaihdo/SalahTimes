68bce8fae67f1ea36789fa742cad958265cdb89a
Zaidh Imran <zaidhimran2000@gmail.com>
2025-10-16 22:10:40 -0230

    package version fixes

--- metro.config.js
+++ metro.config.js
@@ -2,11 +2,11 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(process.cwd());
 
-config.resolver = {
-  ...config.resolver,
-  alias: {
-    '@/': './',
-  },
-};
+// Optimize file watching
+config.watchFolders = [process.cwd()];
+config.resolver.assetExts.push('db'); // Add any custom extensions
+
+// Reduce watched file types
+config.resolver.sourceExts = ['js', 'jsx', 'ts', 'tsx', 'json'];
 
 module.exports = config;
