2e7a0dd0b723456f448cd9fa03e304f12ada449f
Zaidh Imran <zaidhimran2000@gmail.com>
2026-01-17 17:35:29 -0330

    menu secreen titles edited

--- app/contact-us/_layout.tsx
+++ app/contact-us/_layout.tsx
@@ -14,6 +14,7 @@ export default function ContactUsLayout() {
   return (
     <Stack
       screenOptions={({ route }) => ({
+        title: 'Contact Us',
         header: ({ navigation, options, back }) => (
           <View
             style={{
